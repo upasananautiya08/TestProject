819876909ad32f4f91cedb2eaad6cc46e61fc323
upasananautiya08 <52317922+upasananautiya08@users.noreply.github.com>
2020-01-24 17:25:51 +0530

    Add change

--- MyPrograms/src/FirstClass.java
+++ MyPrograms/src/FirstClass.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class FirstClass {
 	public static void main(String[] args)
 	{
-		int num1,num2,num3;
+		int num1,num2,num3,num4;
 		Scanner scan= new Scanner(System.in);
 		System.out.println("Enter a number");
 		num1=scan.nextInt();
@@ -12,7 +12,9 @@ public class FirstClass {
 		num2=scan.nextInt();
 		scan.close();
 		num3= num1*num2;
-		System.out.print("The multiply of the number is =" +num3);			
+		System.out.print("The multiply of the number is =" +num3);
+		num4=num1+num2;
+		System.out.println("the sum is =" + num4);
 		
 	}
 }
